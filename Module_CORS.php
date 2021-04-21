2a132b1d62b6c12341dd15e5ca073d31df6107e8
gizmore <gizmore@wechall.net>
2021-04-21 12:19:10 +0200

    6.10.1-r1032

--- Module_CORS.php
+++ Module_CORS.php
@@ -1,35 +1,51 @@
 <?php
 namespace GDO\CORS;
+
 use GDO\Core\GDO_Module;
-use GDO\Core\Application;
 use GDO\DB\GDT_Checkbox;
 use GDO\Util\Common;
+
 /**
  * Add CORS headers on non cli requests.
  * Optional: Allow any origin. will try a lot of possible working values from request. If cors is set in request vars you can force it.
  * Default to SERVER_NAME.
- * @since 6.07
- * @version 6.09
  * @author gizmore
+ * @version 6.10.1
+ * @since 6.7.0
  */
 final class Module_CORS extends GDO_Module
 {
+    ##############
+    ### Module ###
+    ##############
+    public $module_priority = 10;
+    public function onLoadLanguage() { return $this->loadLanguage('lang/cors'); }
+    
+    ##############
+    ### Config ###
+    ##############
 	public function getConfig()
 	{
-		return array(
+		return [
 			GDT_Checkbox::make('cors_allow_any')->initial('0'),
-		);
+		    GDT_Checkbox::make('cors_allow_creds')->initial('1'),
+		];
 	}
-	
 	public function cfgAllowAny() { return $this->getConfigValue('cors_allow_any'); }
+	public function cfgAllowCredentials() { return $this->getConfigValue('cors_allow_creds'); }
 	
+	############
+	### Init ###
+	############
 	public function onInit()
 	{
-		$app = Application::instance();
-		if (!$app->isCLI())
+	    # Origin
+		hdr("Access-Control-Allow-Origin: " . $this->getOrigin());
+		
+		# Credentials
+		if ($this->cfgAllowCredentials())
 		{
-			@header("Access-Control-Allow-Origin: " . $this->getOrigin());
-			@header("Access-Control-Allow-Credentials: true");
+		    hdr("Access-Control-Allow-Credentials: true");
 		}
 	}
 	
@@ -56,4 +72,5 @@ final class Module_CORS extends GDO_Module
 		}
 		return @$_SERVER['SERVER_NAME'];
 	}
+	
 }
---
+++ lang/cors_en.php
@@ -0,0 +1,5 @@
+<?php
+return [
+    'cfg_cors_allow_any' => 'Allow any domain?',
+    'cfg_cors_allow_creds' => 'Allow credentials to be exchanged?',
+];
